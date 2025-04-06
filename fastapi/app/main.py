c5cea8c210c33b1c7e4584212bc42dca2eb5afa5
lucas.tk <lucas.tk@lucas-tk.local>
2025-04-06 15:19:28 +0900

    log.py -> user_action.py

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.log import router as log_router
+from app.api.user_action import router as user_action_router
 from app.db.models import UserAction
 from app.db.session import Base, engine
 
@@ -17,7 +17,7 @@ app.add_middleware(
 )
 
 # 라우터 등록
-app.include_router(log_router)
+app.include_router(user_action_router)
 
 # ✅ 테이블 자동 생성
 Base.metadata.create_all(bind=engine)
